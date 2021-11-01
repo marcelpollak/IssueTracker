0a51c324761a4956bbcbce6e04084324970624ed
Marcel Pollak <marcel.pollak@outlook.de>
2021-11-01 17:00:52 +0100

    add currentUserService

---
+++ FrontendBlazor/Services/CurrentUserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FrontendBlazor
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            var user = httpContextAccessor.HttpContext?.User;
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("This request does not have an authenticated user.");
+            }
+
+            UserId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public string UserId { get; }
+    }
+}
